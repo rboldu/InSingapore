7c3a30df3f62ca9d2847763979e0aa5bacfa33b0
Sachith Muthukumarana <sachith@ahlab.org>
2016-02-19 12:28:08 +0700

    updated

--- Smart_plug/main.py
+++ Smart_plug/main.py
@@ -1,7 +1,13 @@
 import sys
 from smart_plug import *
+from dali_lights import *
+import time
+
 ip='192.168.81.2'
 port='3480'
+
+dali_ip='192.168.1.250'
+dali_port='50000'
 
 switch4 = smart_plug(ip,port)
 switch4.setId(4)
@@ -37,7 +43,14 @@ for x in range(0, 3):
 p=switch4.getIntensity()
 print p
 
+print '---------------------dali testing------------------------'
 
+dali_group_2=dali_lights(dali_ip,dali_port)
+dali_group_2.setGroup(2)
 
+dali_group_2.setOn()
+
+a=dali_group_2.getGroup()
+print a
 
 
