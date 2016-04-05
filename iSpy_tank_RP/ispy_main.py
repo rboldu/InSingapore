9a802ae7da9029490c1f35de403a2fa370321864
Sachith Muthukumarana <sachith@ahlab.org>
2016-04-05 15:31:53 +0800

    corrected

--- iSpy_tank_RP/ispy_main.py
+++ iSpy_tank_RP/ispy_main.py
@@ -1,14 +1,19 @@
 from ispy_tank import *
 
-ip='10.10.1.1'
-port=8150
-
 ip_rp1='192.168.1.51'
 ip_rp2='192.168.1.50'
 
-ispy_1 = ispy_tank(ip,port,ip_rp1,id=1)
+ispy_2 = ispy_tank(ip_rp2,id=2)
+ispy_1 = ispy_tank(ip_rp1,id=1)
+
+ispy_2.setOn()
+ispy_1.setOn()
+ispy_2.setOn()
 ispy_1.setOn()
 
-ispy_2 = ispy_tank(ip,port,ip_rp2,id=1)
-ispy_2.setOn()
+ispy_1.getId()
+ispy_2.getId()
+
+
+
 
--- iSpy_tank_RP/ispy_tank.py
+++ iSpy_tank_RP/ispy_tank.py
@@ -5,17 +5,12 @@ delay=0.5
 ispy_connection=ispy_connection_rp()
 
 class ispy_tank():
-	def __init__(self,ip,port,ip_rp,id):
-		self.ip=ip
-		self.port=port
-		self.ID=id
+	def __init__(self,ip_rp,id):
+		self.id=id
 		self.ip_rp=ip_rp
-		ispy_connection.setIP(self.ip)
-		ispy_connection.setPORT(self.port)
-		ispy_connection.setIP_RP(self.ip_rp)
 
 	def getId(self):
-		return self.ID
+		return self.id
 
 	def getWhoIm(self):
 		return "iSpy tank"
@@ -27,13 +22,12 @@ class ispy_tank():
 		return -1
 
 	def setOn(self):
-		ispy_connection.forward()
+		ispy_connection.forward(self.ip_rp)
 		time.sleep(delay)
-		ispy_connection.stop()
-		#print self.ip_rp
+		ispy_connection.stop(self.ip_rp)
 
 	def setOff(self):
-		ispy_connection.stop()
+		ispy_connection.stop(self.ip_rp)
 	
 	def setParameter(self,parameter):
 		return -1
@@ -45,24 +39,24 @@ class ispy_tank():
 		return -1
 	
 	def Increase(self,value=30):
-		ispy_connection.forward()
+		ispy_connection.forward(self.ip_rp)
 		time.sleep(delay)
-		ispy_connection.stop()
+		ispy_connection.stop(self.ip_rp)
 	
 	def Decrease(self,value=30):
-		ispy_connection.backward()
+		ispy_connection.backward(self.ip_rp)
 		time.sleep(delay)
-		ispy_connection.stop()
+		ispy_connection.stop(self.ip_rp)
 	
 	def Param_Increase(self,value=30):
-		ispy_connection.turn_right()
+		ispy_connection.turn_right(self.ip_rp)
 		time.sleep(delay)
-		ispy_connection.stop()
+		ispy_connection.stop(self.ip_rp)
 	
 	def Param_Decrease(self,value=30):
-		ispy_connection.turn_left()
+		ispy_connection.turn_left(self.ip_rp)
 		time.sleep(delay)
-		ispy_connection.stop()
+		ispy_connection.stop(self.ip_rp)
 	
 	def copy(self):
 		return -1
